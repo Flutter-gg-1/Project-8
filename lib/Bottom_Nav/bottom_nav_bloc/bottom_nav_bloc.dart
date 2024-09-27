cab9d15267e74da5b550ae375cce81f3c7791471
muhannad <mohndalzaher@gmail.com>
2024-09-27 19:04:04 +0300

    project_strucre finshed

---
+++ lib/Bottom_Nav/bottom_nav_bloc/bottom_nav_bloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+
+part 'bottom_nav_event.dart';
+part 'bottom_nav_state.dart';
+
+class BottomNavBloc extends Bloc<BottomNavEvent, BottomNavState> {
+  List<Widget> views = [
+   
+  ];
+
+  int currentIndex = 0;
+
+  BottomNavBloc() : super(BottomNavInitial()) {
+    on<ChangeEvent>(changeMethod);
+    on<HideBottomNavEvent>((event, emit) => emit(HideBottomNavState()));
+    on<ShowBottomNavEvent>((event, emit) => emit(ShowBottomNavState()));
+  }
+
+  FutureOr<void> changeMethod(ChangeEvent event, Emitter<BottomNavState> emit) {
+    currentIndex = event.index;
+    emit(SuccessChangeViewState(currentIndex));
+  }
+}
