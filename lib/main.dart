5dc7ac7e19c70e0dc69fb60016549b107442affe
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-09-28 01:39:13 +0300

    completed Register and Login Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:onze_cafe/Auth%20Screens/login_screen.dart';
+import 'package:onze_cafe/Auth%20Screens/register_screen.dart';
 import 'package:onze_cafe/Home%20Screen/home_screen.dart';
 import 'package:onze_cafe/services/setup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await setup();
+  // await setup();
   runApp(const MainApp());
 }
 
@@ -13,6 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomeScreen());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: RegisterScreen());
   }
 }
