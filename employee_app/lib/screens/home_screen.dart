7d7643c328b722ca83001385308029e38d48b5a2
Anas Aljuhani <Anas.aljuhani.dev@gmail.com>
2024-10-02 01:23:45 +0300

    home Emp screen ui done

--- employee_app/lib/screens/home_screen.dart
+++ employee_app/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:employee_app/helper/extinsion/size_config.dart';
+import 'package:employee_app/widget/button/home_button.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +7,67 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Top Image
+          Image.asset(
+            'assets/image/background_coffee_top.png',
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset('assets/image/onze_logo_bg.png'),
+              SizedBox(
+                height: context.getHeight(multiply: 0.02),
+              ),
+              HomeButtonEmp(
+                onTap: () {},
+                isIcon: false,
+                height: context.getHeight(multiply: 0.12),
+                width: context.getWidth(multiply: 0.8),
+                color: const Color(0xffCB918B).withOpacity(0.70),
+                text: 'Menu',
+                textSize: 23,
+              ),
+              SizedBox(
+                height: context.getHeight(multiply: 0.02),
+              ),
+              HomeButtonEmp(
+                onTap: () {},
+                isIcon: true,
+                height: context.getHeight(multiply: 0.12),
+                width: context.getWidth(multiply: 0.8),
+                color: const Color(0xffCB918B).withOpacity(0.70),
+                text: 'Orders',
+                textSize: 23,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: context.getHeight(multiply: 0.01786),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: context.getWidth(multiply: 0.08),
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Image.asset('assets/image/man_with_star.png')),
+            ],
+          ),
+          // Bottom Image
+          Image.asset(
+            'assets/image/background_coffee_bottom (2).png',
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
   }
 }
