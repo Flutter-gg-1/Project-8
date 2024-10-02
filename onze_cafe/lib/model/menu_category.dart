9da68a74e8111e4bf5263ca654996c3fc203020d
yara albouq <yaramalbouq@gmail.com>
2024-10-02 14:59:18 +0300

    nenu & category

--- onze_cafe/lib/model/menu_category.dart
+++ onze_cafe/lib/model/menu_category.dart
@@ -1,14 +1,23 @@
 class MenuCategory {
-  String id;
+  String? id;
   String name;
   String description;
   String imgUrl;
   int sortPriority;
 
   MenuCategory(
-      {required this.id,
+      { this.id,
       required this.name,
       required this.description,
       required this.imgUrl,
       required this.sortPriority});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "name": name,
+      "description": description,
+      "imgUrl": imgUrl,
+      "sortPriority": sortPriority,
+    };
+  }
 }
