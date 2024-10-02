4a7686791fd8fee719328cc01f41035c243dc6b4
Basel-75 <basel_1422@outlook.com>
2024-10-02 22:43:57 +0300

    Menu screen logic is complete

--- employee_app/lib/screens/menu/menu_screen.dart
+++ employee_app/lib/screens/menu/menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:employee_app/data_layer/product_layer.dart';
 import 'package:employee_app/helper/extinsion/size_config.dart';
+import 'package:employee_app/models/product_model.dart';
 import 'package:employee_app/models/user_model.dart';
 import 'package:employee_app/screens/profile/profile_screen.dart';
 import 'package:employee_app/services/setup.dart';
@@ -144,60 +145,102 @@ class MenuScreen extends StatelessWidget {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-            horizontal: context.getWidth(multiply: 0.05),
-            vertical: context.getHeight(multiply: 0.15)),
-        child: Center(
-          child: Column(children: [
-            Image.asset('assets/image/menulogo.png'),
-            context.addSpacer(multiply: 0.02),
-            Container(
-              width: context.getWidth(multiply: 0.4),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) => SizedBox(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight * 0.8,
+            child: SingleChildScrollView(
               padding: EdgeInsets.symmetric(
-                vertical: context.getWidth(multiply: 0.02),
-                horizontal: context.getWidth(multiply: 0.02),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: const Color(0xffEFEDEA),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.edit,
-                          color: Color(0xff3D6B7D),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.highlight_remove_rounded,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
+                  horizontal: context.getWidth(multiply: 0.05),
+                  vertical: context.getHeight(multiply: 0.02),
                   ),
-                  Image.asset('assets/image/menuImage.png'),
-                  context.addSpacer(multiply: 0.02),
-                  Text(
-                    'Version: 1.0.0',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
-                        fontFamily: 'Rosarivo'),
-                  ),
-                ],
+              child: Center(
+                child: Column(children: [
+                  Image.asset('assets/image/menulogo.png'),
+                  context.addSpacer(multiply: 0.04),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: GridView.builder(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: locator.menu.length,
+                      itemBuilder: (context, index) => MenuItem(
+                        itemInfo: locator.menu[index],
+                        editFunction: () {},
+                        removeFunction: () {},
+                      ),
+                    ),
+                  )
+                ]),
               ),
             ),
-          ]),
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final Function()? editFunction;
+  final Function()? removeFunction;
+  final ProductModel itemInfo;
+  const MenuItem({
+    super.key,
+    this.editFunction,
+    this.removeFunction,
+    required this.itemInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: context.getWidth(multiply: 0.4),
+      padding: EdgeInsets.symmetric(
+        vertical: context.getWidth(multiply: 0.02),
+        horizontal: context.getWidth(multiply: 0.02),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: const Color(0xffEFEDEA),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: editFunction,
+                icon: const Icon(
+                  Icons.edit,
+                  color: Color(0xff3D6B7D),
+                ),
+              ),
+              IconButton(
+                onPressed: removeFunction,
+                icon: const Icon(
+                  Icons.highlight_remove_rounded,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+          Image.asset('assets/image/menuImage.png'),
+          context.addSpacer(multiply: 0.02),
+          Text(
+            '${itemInfo.name}',
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+                fontFamily: 'Rosarivo'),
+          ),
+        ],
       ),
     );
   }
