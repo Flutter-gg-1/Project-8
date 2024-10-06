92008e249583ed0e417181081a041a03929f4176
amer266030 <amer266030@users.noreply.github.com>
2024-10-06 03:44:12 +0300

    main path

--- onze_cafe/lib/main.dart
+++ onze_cafe/lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:onze_cafe/screens/admin_screens/employee_dashboard/employee_dashboard_screen.dart';
-import 'package:onze_cafe/screens/admin_screens/orders_dashboard/orders_dashboard_screen.dart';
-import 'package:onze_cafe/screens/admin_screens/reports_dashboard/reports_screen.dart';
 import 'package:onze_cafe/screens/landing/landing_screen.dart';
-import 'package:onze_cafe/screens/payment_confirm_screen.dart';
 import 'package:onze_cafe/services/di_container.dart';
 import 'package:onze_cafe/supabase/client/supabase_mgr.dart';
 
@@ -45,7 +41,7 @@ class MainApp extends StatelessWidget {
             theme: ThemeData.light(),
             darkTheme: ThemeData.dark(),
             themeMode: themeMode,
-            home: ReportsScreen(),
+            home: LandingScreen(),
           );
         },
       ),
